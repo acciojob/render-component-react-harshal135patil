393693b882be2130fa8b776ee662f34b90f6493e
Harshu135patil <harsh135235@gmail.com>
2022-12-19 18:12:40 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Root from "./root";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    
+    <div id="main">
+      <Root />
+    </div>
   )
 }
 
---
+++ src/components/root.js
@@ -0,0 +1,8 @@
+const Root = ()=>{
+    return(
+        <div id="root">
+         <p>"I am learning React. My life is getting better. "</p>
+        </div>
+    )
+}
+export default Root
