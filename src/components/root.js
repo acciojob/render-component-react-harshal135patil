b59886ccb0684745627849605ca7c6c748f5a7a9
Harshu135patil <harsh135235@gmail.com>
2023-03-30 15:53:16 +0530

    done

--- src/components/root.js
+++ src/components/root.js
@@ -1,7 +1,7 @@
 const Root = ()=>{
     return(
         <div id="root">
-         <p>"I am learning React. My life is getting better. "</p>
+         <p> I am learning React. My life is getting better.</p>
         </div>
     )
 }
